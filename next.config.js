e93779ce2ed16a77f43a4324874a4612266d4843
Ujjaini Mukherjee <ujjaini@Angshumans-MacBook-Pro.local>
2020-04-15 12:02:13 -0600

    Initial checkin with next.js and spotify search API call

---
+++ next.config.js
@@ -0,0 +1,45 @@
+const withSass = require('@zeit/next-sass')
+const withPlugins = require('next-compose-plugins');
+const withCss = require("@zeit/next-css")
+
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style\/css.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [ // eslint-disable-line
+          (context, request, callback) => { // eslint-disable-line
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ];
+  
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        });
+      }
+      return config;
+    },
+  };
+  
+  module.exports = withPlugins(
+    [
+      [withCss],
+      [
+        withSass,
+        {
+          cssModules: true,
+          cssLoaderOptions: {
+            localIdentName: '[path]___[local]___[hash:base64:5]',
+          },
+        },
+      ],
+    ],
+    nextConfig,
+  );
---
+++ pages/[albums]/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import fetch from 'isomorphic-unfetch'
+import Thumbnail from '../../components/Thumbnail';
+import PropTypes from 'prop-types';
+import "../../styles.css"
+
+
+const Albums = ({albums}) => {
+	const renderAlbums = albums.data.map(item => {
+		return (
+			<li key={item.id}>
+				<Thumbnail
+					imageUrl={item.imageSrc}
+					caption={item.name}
+					artist={item.artist}
+					href={`/albums`}
+					as={`/albums/${item.id}`}
+				/>
+				<button className="add__to__list">
+					Add to List
+				</button>
+			</li>
+		);
+	});
+
+	return (
+		<div className="albums__list">
+			<header>
+				<div className="page__desc">
+					<h1>Your Search Results</h1>
+				</div>
+			</header>			
+			<ul className="albums__grid">
+				{renderAlbums}
+			</ul>
+		</div>
+    )
+};
+
+Albums.getInitialProps = async ({query}) => {
+	try {
+		const res = await fetch(`http://localhost:3000/api/SearchAllAlbums?search=${ query.searchValue}`)
+        const json = await res.json()
+        return { albums: json }
+
+	} catch (error) {
+		return {
+			statusCode: error.response ? error.response.status : 500
+		};
+	}
+};
+
+Albums.propTypes = {
+	albums: PropTypes.object
+}
+
+export default Albums;
