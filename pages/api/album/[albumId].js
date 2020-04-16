35a3b632b3de0dda485ccddb8baf72cd8a825cfb
ujjainimukherjee <c_ujjaini@yahoo.com>
2020-04-16 16:22:16 -0600

    Use spotify-web-api-node instead of node-spotify-api, for pagination

--- pages/api/album/[albumId].js
+++ pages/api/album/[albumId].js
@@ -1,4 +1,5 @@
-const Spotify = require('node-spotify-api');
+//const Spotify = require('node-spotify-api');
+var SpotifyWebApi = require('spotify-web-api-node');
 
 function formatResponse(data) {
     return {
@@ -10,17 +11,48 @@ function formatResponse(data) {
     };
 }
 
+async function getToken (spotify) {
+    const result = await spotify.clientCredentialsGrant()
+    return result.body.access_token
+}
+
+async function initialize(spotify) {
+    const tok = await getToken(spotify)
+    spotify.setAccessToken(tok)
+}
+
+async function getAlbum(spotify, albumId, res) {
+    await initialize(spotify)
+    spotify.getAlbum(albumId).then(function(data) {
+        console.log(data)
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'application/json')
+        res.send(JSON.stringify({ data:formatResponse(data.body)}))
+        res.end();
+    }, function(err) {
+        console.error(err);
+        res.statusCode = 400
+    });
+}
+
 export default (req, res) => {
     const {
         query: { albumId },
     } = req;
 
-    const spotify = new Spotify({
+    /*const spotify = new Spotify({
         id: '467b0f6214aa48f9ab185396b7888ddf',
         secret: '003823169c5c46fa9d18b1bd9b01a309',
+    });*/
+
+    const spotify = new SpotifyWebApi({
+      clientId: '467b0f6214aa48f9ab185396b7888ddf',
+      clientSecret: '003823169c5c46fa9d18b1bd9b01a309'
     });
 
-    spotify
+    getAlbum(spotify, albumId, res)
+
+    /*spotify
         .request(`https://api.spotify.com/v1/albums/${albumId}`)
         .then((data) => {
             res.statusCode = 200;
@@ -30,5 +62,5 @@ export default (req, res) => {
         })
         .catch((err) => {
             console.error('Error occurred: ' + err);
-        });
+        });*/
 };
