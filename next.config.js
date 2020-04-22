bd65e5feed5bc602112c684aa987bac36c625dba
Ujjaini Mukherjee <ujjaini@Angshumans-MacBook-Pro.local>
2020-04-22 15:08:05 -0600

    adding bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
 require('dotenv').config()
+const withPlugins = require('next-compose-plugins');
 const withCSS = require('@zeit/next-css')
-module.exports = withCSS({
+const withBundleAnalyzer = require('@next/bundle-analyzer');
+
+const nextConfig = {
   env: {
     // Reference a variable that was defined in the .env file and make it available at Build Time
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET
 }
-})
+};
+
+
+
+module.exports = withPlugins([
+  withCSS,  
+  [withBundleAnalyzer, 
+  {
+    enabled: process.env.ANALYZE === 'true'
+  }
+]], nextConfig)
+
+// Below code works 
+// module.exports = withBundleAnalyzer(nextConfig)
